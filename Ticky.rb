38121128c046c0e0026e715d497272102b379f76
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-12 09:05:52 +0900

    Update Ticky.rb

--- Ticky.rb
+++ Ticky.rb
@@ -3,7 +3,7 @@ cask "ticky" do
   
     sha256 "46979eb79138df173a593599ee4b93a3de03a06c26bf4bf078db0f6923627c37"
   
-    url "https://github.com/oyeong011/D-Day-MenuBar-macOS/download/v#{version}/Ticky%20v1.0.0.zip"
+    url "https://github.com/oyeong011/D-Day-MenuBar-macOS/releases/download/v#{version}/Ticky%20v1.0.0.zip"
 
     name "Ticky"
     desc "Ticky for your menu bar"
