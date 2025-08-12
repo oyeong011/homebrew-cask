b13c8fd2be5c438fd4042732d303d6c31fcf71c1
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-12 09:20:17 +0900

    Update Ticky.rb

--- Ticky.rb
+++ Ticky.rb
@@ -1,14 +1,14 @@
 cask "ticky" do
   version "1.0.0"
-  
+
   sha256 "46979eb79138df173a593599ee4b93a3de03a06c26bf4bf078db0f6923627c37"
-  
-  # URL에 '/releases/'를 추가했습니다.
-  url "https://github.com/oyeong011/D-Day-MenuBar-macOS/releases/download/v#{version}/Ticky.v1.0.0.zip"
+
+  url "https://github.com/oyeong011/Ticky/releases/download/v#{version}/Ticky.v1.0.0.zip"
 
   name "Ticky"
-  desc "Ticky for your menu bar"
-  homepage "https://github.com/oyeong011/D-Day-MenuBar-macOS"
-  
+  desc "D-Day counter for your menu bar"
+  homepage "https://github.com/oyeong011/Ticky"
+
   app "Ticky.app"
 end
+
