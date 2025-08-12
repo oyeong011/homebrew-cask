c30eb3205b8d468e412d0de8c2f6601260f442a5
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-12 09:31:26 +0900

    Update ticky.rb

--- Casks/ticky.rb
+++ Casks/ticky.rb
@@ -1,7 +1,7 @@
 cask "ticky" do
   version "1.0.0"
 
-  sha256 "46979eb79138df173a593599ee4b93a3de03a06c26bf4bf078db0f6923627c37" # 이 값은 그대로 두세요.
+  sha256 "3aa70cd50a2cf019bd9a67cd6c03bec7a77b622ae0a1d5e7efe749bf1c66f626" # 이 값은 그대로 두세요.
 
   # ⚠️ 중요: URL의 저장소 이름을 'Ticky'로 수정했습니다.
   url "https://github.com/oyeong011/Ticky/releases/download/v#{version}/Ticky.v1.0.0.zip"
